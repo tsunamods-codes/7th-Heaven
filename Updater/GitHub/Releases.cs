d623738543c4b37f620733f10278cb379f95d492
Martin Barker <martin.barker@thisisbeacon.com>
2021-05-24 22:05:44 +0100

    Starting work on auto updater

---
+++ Updater/GitHub/Releases.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.GitHub
+{
+    public class Releases
+    {
+        public enum Channel
+        {
+            Stable = 0,
+            Beta = 1,
+            Alpha = 2,
+            Canary = 3
+        }
+
+        private bool signed = false;
+
+        private string GetUpdateInfoPath()
+        {
+            return Path.Combine(Path.GetTempPath(), "7thheavenupdateinfo.json");
+        }
+
+        private string GetUpdateVersion(string name)
+        {
+            return name.Replace("7thHeaven-v", "");
+        }
+
+        private string GetUpdateReleaseUrl(dynamic assets)
+        {
+            for (int i = 0; i < assets.Count - 1; i++)
+            {
+                string url = assets[i].browser_download_url.Value;
+
+                if (url.Contains("7thHeaven-v"))
+                    return url;
+            }
+
+            return String.Empty;
+        }
+
+        public string GetReleaseJSON(Channel channel)
+        {
+            string filePath = GetUpdateInfoPath();
+            string url;
+            signed = false;
+            switch (channel)
+            {
+                case Channel.Beta:
+                    url = "https://api.github.com/repos/tsunamods-codes/7th-Heaven/releases/tags/beta";
+                    break;
+                case Channel.Alpha:
+                    url = "https://api.github.com/repos/tsunamods-codes/7th-Heaven/releases/tags/alpha";
+                    break;
+                case Channel.Canary:
+                    url = "https://api.github.com/repos/tsunamods-codes/7th-Heaven/releases/tags/canary";
+                    break;
+                default:
+                case Channel.Stable:
+                    url = "https://api.github.com/repos/tsunamods-codes/7th-Heaven/releases/latest";
+                    signed = true;
+                    break;
+            }
+
+            WebClient wc = new WebClient();
+            wc.Headers.Add(HttpRequestHeader.UserAgent, "7th Heaven Updater - Tsunamods Community");
+            string jsontext = wc.DownloadString(url);
+            dynamic release = JValue.Parse(jsontext);
+            foreach(dynamic asset in release.assets)
+            {
+                if (signed)
+                {
+                    string name = asset.name.ToString();
+                    if (name.ToLower().IndexOf("signed") > -1)
+                    {
+                        return asset.browser_download_url.ToString();
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Updater/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Updater.GitHub;
+
+namespace Updater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string[] args;
+
+        public MainWindow()
+        {
+            this.Initialized += MainWindow_Initialized;
+            InitializeComponent();
+        }
+
+        private void MainWindow_Initialized(object sender, EventArgs e)
+        {
+            Releases releases = new Releases();
+            string downloadUrl = releases.GetReleaseJSON(Releases.Channel.Stable);
+            if (downloadUrl != null)
+            {
+                Progress_Text.Content = String.Format("Fetching: {0}", downloadUrl.Split(new char[] { '/' }).Last());
+            }
+            else
+            {
+                // something went wrong close the updater.exe
+            }
+
+            Download download = new Download();
+            download.downloadProgressChanged += Download_downloadProgressChanged;
+            download.downloadFinished += Download_downloadFinished;
+            download.downloadFailed += Download_downloadFailed;
+            download.downloadToTemp(downloadUrl);
+        }
+
+        private void Download_downloadFailed()
+        {
+            Progress_Text.Content = String.Format("Downloading Failed");
+            // something went wrong close the updater.exr
+        }
+
+        private void Download_downloadFinished(string path)
+        {
+            Progress_Text.Content = String.Format("Downloading Finished: {0}", path.Split(new char[] { '\\' }).Last());
+            ProgressBar.Value = 100;
+            Console.WriteLine(path);
+        }
+
+        private void Download_downloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e)
+        {
+            ProgressBar.Value = e.ProgressPercentage;
+        }
+    }
+}
