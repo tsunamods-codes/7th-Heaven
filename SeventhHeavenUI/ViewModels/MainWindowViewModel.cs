30f7bdd68a7a4d3955e876ad38765d0339e088bf
Martin Barker <martin.barker@thisisbeacon.com>
2021-06-08 22:25:55 +0100

    Now using the saved setting for updating.

--- SeventhHeavenUI/ViewModels/MainWindowViewModel.cs
+++ SeventhHeavenUI/ViewModels/MainWindowViewModel.cs
@@ -732,7 +732,7 @@ namespace SeventhHeavenUI.ViewModels
                 {
                     System.Threading.Thread.Sleep(5000); // wait 5 seconds after init before checking for update to let UI render
                     Sys.Message(new WMessage(ResourceHelper.Get(StringKey.CheckingForUpdates), WMessageLogLevel.LogOnly));
-                    updater.CheckForUpdates(Updater.GitHub.Releases.Channel.Stable);
+                    updater.CheckForUpdates(Sys.Settings.UpdateChannel);
                 });
             }
         }
